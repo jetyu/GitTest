41c03c35130ea7ad0e792ecfe05f263e9404f8b0
Jet Yu <jetyu91@gmail.com>
2020-03-03 14:24:22 +0800

    add university

--- src/demo/TestDto.java
+++ src/demo/TestDto.java
@@ -1,33 +1,46 @@
 package demo;
 
 public class TestDto {
-
+	String university;
 	String name;
 	int age;
 	float price;
+
+	public String getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public float getPrice() {
 		return price;
 	}
+
 	public void setPrice(float price) {
 		this.price = price;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "TestDto [name=" + name + ", age=" + age + ", price=" + price + "]";
 	}
-	
-	
+
 }
