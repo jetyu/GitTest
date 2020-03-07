220249be5b8d4cd023aa15c9ae430019562a588a
jetyu <jetyu@users.noreply.github.com>
2020-03-07 20:56:06 +0800

    Update SpellChecker.java

--- src/demo1/SpellChecker.java
+++ src/demo1/SpellChecker.java
@@ -4,10 +4,14 @@ public class SpellChecker {
 
 	public static void checkSpelling() {
 		System.out.println("SpellChecker的checkSpelling方法");
+	}  
+	
+	public void test(){
+		System.out.println("Test方法");
 	}
 
 	public static void main(String[] args) {
-		 
+		 test();
 		checkSpelling();
 	}
 }
