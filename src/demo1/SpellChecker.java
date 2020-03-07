f60b79aca9487e29266752d6d32f7925cbd7dcd7
Jet Yu <jetyu91@gmail.com>
2020-03-07 20:53:32 +0800

    bean add

---
+++ src/demo1/SpellChecker.java
@@ -0,0 +1,13 @@
+package demo1;
+
+public class SpellChecker {
+
+	public static void checkSpelling() {
+		System.out.println("SpellChecker的checkSpelling方法");
+	}
+
+	public static void main(String[] args) {
+		 
+		checkSpelling();
+	}
+}
