84dd87fa07e2405537c13fb9bdff7f1605c7be8f
Jet Yu <jetyu91@gmail.com>
2020-03-09 14:34:03 +0800

    99

--- src/demo1/SpellChecker.java
+++ src/demo1/SpellChecker.java
@@ -9,19 +9,16 @@ public class SpellChecker {
 	public void test(){
 		System.out.println("Test方法");
 	}
-	
-	public void test() {
-		System.out.println("Test方法");
-	}
+ 
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+ 
 	 	checkSpelling();	
 		SpellChecker spellChecker = new SpellChecker();
 		spellChecker.test();
-=======
-		 test();
+ 
+		
 		checkSpelling();
->>>>>>> branch 'master' of https://github.com/jetyu/GitTest.git
+ 
 	}
 }
